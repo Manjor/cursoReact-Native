e226bfb9440e72300d6a90e6ca421f539ca9d2da
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-25 20:16:27 -0300

    Criando Estilo

--- exercicios/components/Simples.js
+++ exercicios/components/Simples.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View,Text } from 'react-native'
+import Padrao from '../styles/Padrao'
 
 export default props => [
-        <Text key={1}>{props.texto}</Text>,
-        <Text key={2}>{props.texto}</Text>
+        <Text style={[Padrao.ex]} key={1}>{props.texto}</Text>,
+        <Text style={Padrao.ex} key={2}>{props.texto}</Text>
 ]
     
     

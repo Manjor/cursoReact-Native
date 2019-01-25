74395967699abd2fe0671694ff21014e786f7f9b
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-01-25 20:11:02 -0300

    Primeiro Componente

--- exercicios/App.js
+++ exercicios/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Simples from './components/Simples'
 
 export default class App extends React.Component{
   render(){
 	return (
 		<View style={styles.container}> 
-			<Text style={styles.f20}>App!</Text>
+			<Simples texto='Flexível!!!'/>
 		</View>
 	)
   }
@@ -16,8 +17,5 @@ const styles =  StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
-	},
-	f20:{
-		fontSize: 20
 	}
 })
---
+++ exercicios/components/Simples.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+export default props => [
+        <Text key={1}>{props.texto}</Text>,
+        <Text key={2}>{props.texto}</Text>
+]
+    
+    

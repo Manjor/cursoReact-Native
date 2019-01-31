816af3ac922168acab5d232f706e19878fba2b61
manjor <manoeltavares.jr@gmail.com>
2019-01-31 15:57:59 -0300

    Lista de Alunos #2

--- exercicios/src/components/ListaFlex.js
+++ exercicios/src/components/ListaFlex.js
@@ -26,7 +26,14 @@ const itemEstilo = {
     height: 50,
     backgroundColor: '#DDD',
     borderWidth: 0.5,
-    borderColor: '#222'
+    borderColor: '#222',
+    
+    //Flex
+    // alignItems: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
 }
 
 export const Aluno = props =>

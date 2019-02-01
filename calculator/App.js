2b7b1c6e81aa98aea4a57d8f7cf9d7fca375f41f
manjor <manoeltavares.jr@gmail.com>
2019-02-01 20:06:55 -0300

    Correção Calculadora #1

--- calculator/App.js
+++ calculator/App.js
@@ -16,6 +16,7 @@ export default class App extends Component{
   state = {...initialState}
 
   addDigit = n =>{
+    console.debug(typeof this.state.displayValue)
     if(n === '.' && this.state.displayValue.includes('.')){
       return
     }
@@ -54,7 +55,7 @@ export default class App extends Component{
 
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         //clearDisplay: !equals,

d41e2786766ad1fecf43f5ee4c55ff1a84383856
manjor <manoeltavares.jr@gmail.com>
2019-02-02 12:25:21 -0300

    Componente Campo (Field) #2

--- mines/App.js
+++ mines/App.js
@@ -12,6 +12,10 @@ export default class App extends Component {
           {params.getRowsAmount()}X{params.getColumnsAmount()}
         </Text>
         <Field/>   
+        <Field opened nearMines={1}/>   
+        <Field opened nearMines={2}/>   
+        <Field opened nearMines={3}/>   
+        <Field opened nearMines={6}/>   
       </View>
     );
   }

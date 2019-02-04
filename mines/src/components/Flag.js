17da4a60e7dd39fc05d86485ab2ac7fe73dbe83a
manjor <manoeltavares.jr@gmail.com>
2019-02-04 13:45:46 -0300

    Componente Bandeira (Flag) #2

--- mines/src/components/Flag.js
+++ mines/src/components/Flag.js
@@ -6,10 +6,10 @@ import {
 export default props => {
     return (
         <View style={styles.container}>
-            <View style={styles.flagpole}/>
-            <View style={styles.flag}/>
-            <View style={styles.base1}/>
-            <View style={styles.base2}/>
+            <View style={[styles.flagpole, props.bigger ? styles.fladpoleBigger : null]}/>
+            <View style={[styles.flag, props.bigger ? styles.flagBigger : null]}/>
+            <View style={[styles.base1 , props.bigger ? styles.base1Bigger: null ]}/>
+            <View style={[styles.base2, props.bigger ? styles.base2Bigger: null]}/>
         </View>
     )
 }
@@ -46,5 +46,28 @@ const styles = StyleSheet.create({
         backgroundColor: '#222',
         marginLeft: 5,
         marginTop: 12
+    },
+    fladpoleBigger:{
+        height:20,
+        width: 4,
+        marginLeft: 16
+    },
+    flagBigger:{
+        height:10,
+        width: 14,
+        marginLeft: 3,
+    },
+    base1Bigger:{
+        height: 4,
+        width: 12,
+        marginTop:20,
+        marginLeft: 12,
+    },
+    base2Bigger:{
+        height: 4,
+        width: 20,
+        marginLeft: 8,
+        marginTop: 24
     }
+
 })

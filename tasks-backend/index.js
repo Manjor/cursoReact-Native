86dfe3d5ff3122beaeb99497bba051e4d9738377
Manjor <manoeltavares.jr@gmail.com>
2019-02-18 15:18:41 -0300

    Tasks Backend: Middlewares e DB com Knex #2

--- tasks-backend/index.js
+++ tasks-backend/index.js
@@ -1,4 +1,12 @@
 const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares')
+    .into(app)
+
+app.db = db
 
 app.listen(3000,() =>{
     console.log('Backend Executando')

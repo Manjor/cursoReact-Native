cedd6b8b348a5beee722d42aad23bf9657ff3708
Manjor <manoeltavares.jr@gmail.com>
2019-02-18 15:13:14 -0300

    Tasks Backend: BD Migrations

--- tasks-backend/migrations/20190218150513_create_table_users.js
+++ tasks-backend/migrations/20190218150513_create_table_users.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', table =>{
+        table.increments('id').primary()
+        table.string('name').notNullable()
+        table.string('email').notNullable().unique()
+        table.string('password').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('users')
 };
--- tasks-backend/migrations/20190218150535_create_tasks.js
+++ tasks-backend/migrations/20190218150535_create_tasks.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('tasks', table =>{
+        table.increments('id').primary()
+        table.string('desc').notNullable()
+        table.dateTime('estimateAt')
+        table.dateTime('doneAt')
+        table.integer('userId').references('id')
+            .inTable('users').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    knex.schema.dropTable('tasks')
 };

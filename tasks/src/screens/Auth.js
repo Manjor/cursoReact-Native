bc1055e84ea4da700e313040ed9ff54d76c22487
Manjor <manoeltavares.jr@gmail.com>
2019-02-22 15:18:14 -0300

    Tasks: Validar Formulário

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -22,35 +22,53 @@ export default class Auth extends Component {
         password: '',
         confirmPassword: '',
     }
+    signin = async () =>{
+        try{
+            const res = await axios.post(`${server}/signin`,{
+                email: this.state.email,
+                password: this.state.password
+            })
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            this.props.navigation.navigate('Home')
+        }catch (e) {
+            Alert.alert('Erro', 'Usuário ou Senha Invalidos')
+        }
+    }
+    signup = async () =>{
+        try{
+            await axios.post(`${server}/signup`,{
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+            Alert.alert('Sucesso', "Usuário Cadastrado.")
+        }catch (e) {
+            showError(e)
+        }
+    }
 
-    signinOrSignup = async () => {
+    signinOrSignup = () => {
         if (this.state.stageNew) {
-            try{
-                await axios.post(`${server}/signup`,{
-                    name: this.state.name,
-                    email: this.state.email,
-                    password: this.state.password,
-                    confirmPassword: this.state.confirmPassword
-                })
-                Alert.alert('Sucesso', "Usuário Cadastrado.")
-            }catch (e) {
-                showError(e)
-            }
+            this.signup()
         } else {
-            try{
-                const res = await axios.post(`${server}/signin`,{
-                    email: this.state.email,
-                    password: this.state.password
-                })
-                axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-                this.props.navigation.navigate('Home')
-            }catch (e) {
-                Alert.alert('Erro', 'Usuário ou Senha Invalidos')
-            }
+            this.signin()
         }
     }
 
     render() {
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6 )
+
+        if(this.state.stageNew){
+            validations.push(this.state.name && this.state.name.trim())
+            validations.push(this.state.confirmPassword)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((all, v) => all && v)
+
         return (
             <ImageBackground source={backgroundImage}
                              style={styles.background}>
@@ -73,8 +91,8 @@ export default class Auth extends Component {
                     <AuthInput icon="asterisk" secureTextEntry={true} placeholder='Cofirmação' style={styles.input}
                                value={this.state.confirmPassword}
                                onChangeText={confirmPassword => this.setState({ confirmPassword })}/>}
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity disabled={!validForm} onPress={this.signinOrSignup}>
+                        <View style={[styles.button, !validForm ? {backgroundColor: '#AAA'}: {}]}>
                             <Text style={styles.buttonText}>
                                   {this.state.stageNew ? 'Register' : 'Entrar'}
                             </Text>

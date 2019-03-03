9c90ae5f36f869adeba655f457afdc1c191877cf
Manoel Tavares <manoeltavares.jr@gmail.com>
2019-03-03 10:22:36 -0300

    Instagram Clone: Componente Post

--- lambe/App.js
+++ lambe/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
 import Header from './src/components/Header'
+import { View, Text } from 'react-native'
+import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
     return (
-      <Header/>
+      <View>
+        <Header/>
+        <Post image={require('./assets/imgs/fence.jpg')} />  
+      </View>
+      
     )
   }
 }

a95c3f83c00edf461e37241e7f0b873e228dd839
Manoel Tavares <manoeltavares.jr@gmail.com>
2019-03-06 13:17:02 -0300

    Instagram Clone : Login com Redux #2

--- lambe/index.js
+++ lambe/index.js
@@ -1,6 +1,18 @@
+import React from 'react'
+import { Provider } from 'react-redux'
 import {AppRegistry} from 'react-native';
 import Feed from './src/screens/Feed'
 import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+import storeConfig from './src/store/storeConfig'
+
+const store = storeConfig()
+const Redux = () =>(
+    <Provider store={store}>
+        <Navigator/>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Redux);
+Redux

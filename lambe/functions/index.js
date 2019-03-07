9031e116e93b431efcdaafbbf8ef4e81b71c02d9
Manoel Tavares <manoeltavares.jr@gmail.com>
2019-03-07 13:51:54 -0300

    Instagram Clone: Usando Firebase Storage #3

--- lambe/functions/index.js
+++ lambe/functions/index.js
@@ -11,6 +11,34 @@ const storage = new Storage({
 
 exports.uploadImage = functions.https.onRequest((request, response) => {
     cors(request,response, () =>{
-        
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg', request.body.image, 'base64')
+
+            const bucket = storage.bucket('lambe-manjor.appspot.com')
+            const id = uuid()
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',
+                destination: `/posts/${id}.jpg`,
+                metadata: {
+                    metadata:{
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            }, (err, file) =>{
+                if (err) {
+                    console.log(err)
+                    return response.status(500).json({ error: err })
+                }else{
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+                        + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                        return response.status(201).json({ imageUrl :imageUrl })
+                }
+            })
+        } catch (error) {
+            console.log(error)
+            return response.status(500).json({ error: error })
+        }
     })
 });

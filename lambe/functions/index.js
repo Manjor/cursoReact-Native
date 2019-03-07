6503819b593909658f2556ba71d769d418fe8713
Manoel Tavares <manoeltavares.jr@gmail.com>
2019-03-07 11:41:24 -0300

    Instagram Clone: Usando Firebase Storage #2

--- lambe/functions/index.js
+++ lambe/functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId:'lambe-manjor',
+    keyFilename: 'lambe-manjor.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    
+
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request,response, () =>{
+        
+    })
 });
